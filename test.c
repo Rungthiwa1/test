9276b306a473a7aa7b1b93aa97cbd01d1ca89a80
Rungthiwa Yawichai <yawichai@celestica.com>
2022-07-27 10:54:28 +0700

    test

---
+++ test.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+/**
+* @wi.implements th26015515/TH26-544:164 Returns b
+*/
+int b(void){
+    return 0;
+}
+
+/**
+* @wi.implements th26015515/TH26-545:164 Returns a
+*/
+int a(void){
+    return 0;
+}
+
+int main() {
+    // Write C code here
+    a();
+    b();
+    printf("Hello world");
+
+    return 0;
+}
